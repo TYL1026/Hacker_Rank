298f998620aa64ef42a5d890b60c8b92e186e1eb
TonyL <65515982+TYL1026@users.noreply.github.com>
2021-05-15 16:24:58 -0700

    Create array.js

---
+++ 10 Days of Javascript/Day 3/array.js
@@ -0,0 +1,43 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    nums = nums.sort(function(a,b){return b-a})
+    const largest = nums[0]
+    let second = largest
+    let i = 0
+    while (largest == second){
+        if (nums[i] < largest){
+            second = nums[i]
+        }
+        i+=1
+    }
+    return second
+}
