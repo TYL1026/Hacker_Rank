d72e0b8239c602d8f3297e032025435d8915cc95
TonyL <65515982+TYL1026@users.noreply.github.com>
2021-05-15 16:35:14 -0700

    Create try_catch.js

---
+++ 10 Days of Javascript/Day 3/try_catch.js
@@ -0,0 +1,40 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the reverseString function
+ * Use console.log() to print to stdout.
+ */
+function reverseString(s) {
+    let result = []
+    try{
+        const num = s.split('')
+        result = num.reverse()
+        console.log(result.join(''))
+    }catch{
+        console.log("s.split is not a function ")
+        console.log(s)
+    }
+}
+
