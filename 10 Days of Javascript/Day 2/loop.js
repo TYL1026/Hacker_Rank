c514039b1b2feb8716ab423a9d20b9c3f15d181e
TonyL <65515982+TYL1026@users.noreply.github.com>
2021-05-15 15:55:51 -0700

    Update loop.js

--- 10 Days of Javascript/Day 2/loop.js
+++ 10 Days of Javascript/Day 2/loop.js
@@ -27,26 +27,19 @@ function readLine() {
  * Print your output using 'console.log()'.
  */
 function vowelsAndConsonants(s) {
-    const vol = ['a','e','i','o','u']
-    const result = []
-    var repeat = 0
-    for( var i = 0 ; i < s.length; i ++ ){
-        repeat = 0
-        vol.forEach(e => {
-                if (s[i] == e && repeat == 0){
-                    console.log(e)
-                    repeat = 1
-                }
-            }
-        )
-        if (repeat == 0){
-            result.push(s[i])
-        }
+    const vowels = 'aeiou';
+    var consonants = '';
     
+    for(var i = 0; i < s.length; i++) {
+       if (vowels.includes(s[i])) {
+           console.log(s[i]);
+       }
+       else {
+           consonants += s[i] + '\n';
+       }
     }
-    for( var i = 0 ; i < result.length; i ++ ){
-        console.log(result[i])
-    }
+    
+    console.log(consonants.trim());
 }
 
 
