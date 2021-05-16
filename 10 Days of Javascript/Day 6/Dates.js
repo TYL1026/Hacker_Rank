995fa2b40142c2dd844a35ede18b17964d73d0b5
TonyL <65515982+TYL1026@users.noreply.github.com>
2021-05-15 22:10:23 -0700

    Update Dates.js

--- 10 Days of Javascript/Day 6/Dates.js
+++ 10 Days of Javascript/Day 6/Dates.js
@@ -1,1 +1,33 @@
 
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+function getDayName(dateString) {
+    let dayName;
+    // Write your code here
+    const date = new Date(dateString)
+    return new Intl.DateTimeFormat('en-Us', {weekday: 'long'}).format(date)
+}
+
